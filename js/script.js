240df5df609659ff1930b29b76371bf68c5df18d
mayanggsarinr <mayanggsarinr.22@gmail.com>
2025-04-16 08:23:21 +0700

    update js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-document.getElementsByTagName('body')
-[0].style.backgroundColor = 'pink';
+document.getElementsByTagName('body')[0].style.backgroundColor = 'yellow';
 
 // variable
 let nama = "Ahmad";
 let umur = 20;
-let statusMahasiswa = true;
 
 document.getElementById('nama').innerHTML = "Nama saya : " + nama;
 document.getElementById('umur').innerHTML = `Umur : ${umur}`;
@@ -12,10 +10,9 @@ document.getElementById('umur').innerHTML = `Umur : ${umur}`;
 let mahasiswa = [
     "Ahmad", "Budi", "Caca", "Dedi"
 ];
-
-mahasiswa.forEach( (mhs) => { //cara menampilkan isi dari array
-    document.getElementById('mahasiswa').innserHTML += `<li>${mhs}</li>`
-} )
+mahasiswa.forEach( (mhs) => {
+    document.getElementById('mahasiswa').innerHTML += `<li>${mhs}</li>`;
+} );
 
 let nilai = [
     {nama: "Ahmad", nilai: 80},
@@ -23,17 +20,16 @@ let nilai = [
     {nama: "Caca", nilai: 90},
     {nama: "Dedi", nilai: 85}
 ];
-
 nilai.forEach( (data) => {
     document.getElementById('nilai').innerHTML += `<tr>
         <td>${data.nama}</td>
-        <td>${data.nama}</td>
+        <td>${data.nilai}</td>
     </tr>`;
-});
+} );
 
-fetch('https://dummyjson.com/quotes')
-.then(res => res.json())
-.then(data => { //tampilkan data
-    console.log(data.quotes) 
-    data.quotes.forEach( (q) => {document.getElementById('quotes').innerHTML += '<li> ${q.quote} - ${q.author} </li>` } )
-});
+fetch('https://dummyjson.com/quotes') // akses API quotes
+.then(res => res.json()) // ubah data ke json
+.then(data => { // tampilkan data
+    console.log(data.quotes)
+    data.quotes.forEach( (q) => { document.getElementById('quotes').innerHTML += `<li>${q.quote} - ${q.author}</li>` } )
+ });
